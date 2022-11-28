29607d3534d36c22af2e575fbab705ce90c767bf
Numero 3 <notnumero3@gmail.com>
2022-11-28 23:07:33 +0100

    Adding Routes

---
+++ kasa/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <Link to='/'>Acceuil</Link>
+            <Link to='/apartement'>Appartement</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/error'>Error_dev</Link>
+        </nav>
+    )
+}
---
+++ kasa/src/components/pages/Apartment.js
@@ -0,0 +1,3 @@
+export const Apartement = () => {
+    return <div>Apartement Page</div>
+}
