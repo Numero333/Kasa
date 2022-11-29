aa0097293ff1e3a89752933f1def60668b06b9b7
Numero 3 <notnumero3@gmail.com>
2022-11-29 22:47:37 +0100

    refact

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,20 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
-import { About } from './components/pages/About';
-import { Apartement } from './components/pages/Apartment';
-import { Error } from './components/pages/Error';
-import { Home } from "./components/pages/Home"
 import { Navbar } from './components/Navbar';
+import { Router } from './components/Router';
 
 function App() {
   return (
     <>
     <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/Apartement' element={<Apartement />}></Route>
-      <Route path='/About' element={<About />}></Route>
-      <Route path='*' element={<Error />}></Route>
-    </Routes>
+    <Router />
     </>
   )
 }
---
+++ kasa/src/components/Router.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import { About } from '../pages/About';
+import { Apartement } from '../pages/Apartment';
+import { Error } from '../pages/Error';
+import { Home } from "../pages/Home"
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/Apartement" element={<Apartement />}></Route>
+      <Route path="/About" element={<About />}></Route>
+      <Route path="*" element={<Error />}></Route>
+    </Routes>
+  );
+};
