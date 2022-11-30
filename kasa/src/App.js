95da615b8c7affd018619fe6c4b0ee7f0dc65af9
Numero 3 <notnumero3@gmail.com>
2022-11-30 22:17:32 +0100

    Adding footer

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,11 +1,13 @@
 import { Header } from './components/Navbar';
 import { Router } from './components/Router';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <>
     <Header />
     <Router />
+    <Footer />
     </>
   )
 }
