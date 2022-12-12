9f38b40fedbf3366e2840a17c9c9286522fd1999
Numero 3 <notnumero3@gmail.com>
2022-12-12 16:12:43 +0100

    Update

--- kasa/src/components/Banner.jsx
+++ kasa/src/components/Banner.jsx
@@ -1,11 +1,18 @@
 import styles from "../style/Banner.module.css";
-import banner from "../assets/images/banner.webp";
 
 export const Banner = (props) => {
   return (
     <div className={styles.bannerWrapper}>
       <img className={styles.bannerImage} src={props.src} alt={props.alt} />
-      <p className={styles.bannerText}>{props.text}</p>
+      {props.text ? <p className={styles.bannerText}>{props.text}</p> : null}
     </div>
   );
 };
+
+/* ----------------------------------------------------------------
+This component is responsible of the banner, 
+<Banner /> takes 3 props :
+- Src
+- Alt
+- Optional Text
+----------------------------------------------- */
