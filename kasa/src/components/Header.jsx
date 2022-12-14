6de1b56821436db884e34425dd045fe851588998
Numero 3 <notnumero3@gmail.com>
2022-12-14 15:46:22 +0100

    Handling Nav Underline

--- kasa/src/components/Header.jsx
+++ kasa/src/components/Header.jsx
@@ -3,11 +3,15 @@ This component is responsible of Header,
 <Header /> contains the navigation tab 
 ----------------------------------------------- */
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "../style/Navbar.module.css";
 import logo from "../assets/images/logo.svg";
 
 export const Header = () => {
+  const location = useLocation();
+  const isHome = String(location.pathname) === "/";
+  const isAbout = String(location.pathname) === "/about";
+
   return (
     <header className={styles.wrapper}>
       <Link to="/">
@@ -16,12 +20,18 @@ export const Header = () => {
       <nav style={styles.nav}>
         <ul className={styles.ul}>
           <li className={styles.li}>
-            <Link className={styles.home} to="/">
+            <Link
+              className={`${styles.home} ${isHome ? styles.active : null}`}
+              to="/"
+            >
               Acceuil
             </Link>
           </li>
           <li className={styles.li}>
-            <Link className={styles.about} to="/about">
+            <Link
+              className={`${styles.about} ${isAbout ? styles.active : null}`}
+              to="/about"
+            >
               A propos
             </Link>
           </li>
